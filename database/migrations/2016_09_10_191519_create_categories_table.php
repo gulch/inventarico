73a0c83d51b2d828b823335c710cdb0706e597b4
gulch <gulch958089>
2016-09-10 19:19:59 +0300

    add model and migration for Category table

---
+++ database/migrations/2016_09_10_191519_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('id__User')->unsigned();
+            $table->timestamps();
+            $table->foreign('id__User')->references('id')->on('User')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Category');
+    }
+}
