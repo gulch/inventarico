511a07b220db4288fa836e57569dafd4d292330c
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2021-04-11 16:11:02 +0300

    add favicon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -19,6 +19,17 @@
 
     {{-- jQuery --}}
     <script src="/assets/vendor/jquery/3.5.1/jquery.min.js"></script>
+
+    {{-- Favicon --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
 {{-- Menu --}}
