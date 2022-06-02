8fb5f31ff6c51b00eea6afb8c691b06f6d1cacdc
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2022-06-02 19:48:15 +0300

    refactor routes

--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,62 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ItemsController;
+use App\Http\Controllers\OperationTypesController;
+use App\Http\Controllers\OperationsController;
+use App\Http\Controllers\PhotosController;
 use Illuminate\Routing\Router;
 
 /**
  * @var Router $router
  */
-$router->get('/', 'HomeController@index');
+$router->get('/', [HomeController::class, 'index']);
 $router->auth();
 
 $router->group(['middleware' => 'auth'], function () use ($router) {
-    $router->get('dashboard', 'DashboardController@index');
+    $router->get('dashboard', [DashboardController::class, 'index']);
 
     /* Photos */
-    $router->get('photos', 'PhotosController@index');
-    $router->get('photos/{id}/edit', 'PhotosController@edit');
-    $router->patch('photos/{id}', 'PhotosController@update');
-    $router->delete('photos/{id}', 'PhotosController@destroy');
-    $router->post('photos/upload', 'PhotosController@upload');
-    $router->post('photos/upload/getid', 'PhotosController@uploadAndCreate');
-    $router->post('photos/all/list', 'PhotosController@getAllImagesList');
+    $router->get('photos', [PhotosController::class, 'index']);
+    $router->get('photos/{id}/edit', [PhotosController::class, 'edit']);
+    $router->patch('photos/{id}', [PhotosController::class, 'update']);
+    $router->delete('photos/{id}', [PhotosController::class, 'destroy']);
+    $router->post('photos/upload', [PhotosController::class, 'upload']);
+    $router->post('photos/upload/getid', [PhotosController::class, 'uploadAndCreate']);
+    $router->post('photos/all/list', [PhotosController::class, 'getAllImagesList']);
 
     /* Categories */
-    $router->get('categories', 'CategoriesController@index');
-    $router->get('categories/create', 'CategoriesController@create');
-    $router->get('categories/{id}/edit', 'CategoriesController@edit');
-    $router->post('categories', 'CategoriesController@store');
-    $router->patch('categories/{id}', 'CategoriesController@update');
-    $router->delete('categories/{id}', 'CategoriesController@destroy');
+    $router->get('categories', [CategoriesController::class, 'index']);
+    $router->get('categories/create', [CategoriesController::class, 'create']);
+    $router->get('categories/{id}/edit', [CategoriesController::class, 'edit']);
+    $router->post('categories', [CategoriesController::class, 'store']);
+    $router->patch('categories/{id}', [CategoriesController::class, 'update']);
+    $router->delete('categories/{id}', [CategoriesController::class, 'destroy']);
 
     /* Operation Types */
-    $router->get('operation-types', 'OperationTypesController@index');
-    $router->get('operation-types/create', 'OperationTypesController@create');
-    $router->get('operation-types/{id}/edit', 'OperationTypesController@edit');
-    $router->post('operation-types', 'OperationTypesController@store');
-    $router->patch('operation-types/{id}', 'OperationTypesController@update');
-    $router->delete('operation-types/{id}', 'OperationTypesController@destroy');
+    $router->get('operation-types', [OperationTypesController::class, 'index']);
+    $router->get('operation-types/create', [OperationTypesController::class, 'create']);
+    $router->get('operation-types/{id}/edit', [OperationTypesController::class, 'edit']);
+    $router->post('operation-types', [OperationTypesController::class, 'store']);
+    $router->patch('operation-types/{id}', [OperationTypesController::class, 'update']);
+    $router->delete('operation-types/{id}', [OperationTypesController::class, 'destroy']);
 
     /* Items */
-    $router->get('items', 'ItemsController@index')->name('available-items');
-    /*$router->get('items/archive', 'ItemsController@indexArchive')->name('archived-items');*/
-    $router->get('items/create', 'ItemsController@create');
-    $router->get('items/{id}/edit', 'ItemsController@edit');
-    $router->get('items/{id}/show', 'ItemsController@show');
-    $router->post('items', 'ItemsController@store');
-    $router->patch('items/{id}', 'ItemsController@update');
-    $router->delete('items/{id}', 'ItemsController@destroy');
+    $router->get('items', [ItemsController::class, 'index'])->name('available-items');
+    $router->get('items/create', [ItemsController::class, 'create']);
+    $router->get('items/{id}/edit', [ItemsController::class, 'edit']);
+    $router->get('items/{id}/show', [ItemsController::class, 'show']);
+    $router->post('items', [ItemsController::class, 'store']);
+    $router->patch('items/{id}', [ItemsController::class, 'update']);
+    $router->delete('items/{id}', [ItemsController::class, 'destroy']);
 
     /* Operations */
-    $router->get('operations', 'OperationsController@index');
-    $router->get('operations/create/{id__Item}', 'OperationsController@create');
-    $router->get('operations/{id}/edit', 'OperationsController@edit');
-    $router->post('operations', 'OperationsController@store');
-    $router->patch('operations/{id}', 'OperationsController@update');
-    $router->delete('operations/{id}', 'OperationsController@destroy');
+    $router->get('operations', [OperationsController::class, 'index']);
+    $router->get('operations/create/{id__Item}', [OperationsController::class, 'create']);
+    $router->get('operations/{id}/edit', [OperationsController::class, 'edit']);
+    $router->post('operations', [OperationsController::class, 'store']);
+    $router->patch('operations/{id}', [OperationsController::class, 'update']);
+    $router->delete('operations/{id}', [OperationsController::class, 'destroy']);
 });
