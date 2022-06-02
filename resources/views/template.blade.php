38a7c9347607149a6eb66dd048cdd2e0604689f6
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2022-06-02 19:29:43 +0300

    added SVG favicon, logo

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -21,9 +21,10 @@
     <script src="/assets/vendor/jquery/3.6.0/jquery.min.js"></script>
 
     {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png">
     <link rel="manifest" href="/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="theme-color" content="#ffffff">
 </head>
 <body>
