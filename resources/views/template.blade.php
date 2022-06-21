813bb10b17151329d6e2a41ff0dd05a29d46fac4
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2022-06-21 16:05:28 +0300

    rename site.webmanifest -> app.webmanifest

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -24,7 +24,7 @@
     <link rel="icon" href="/favicon.ico" sizes="any">
     <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png">
-    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="manifest" href="/favicon/app.webmanifest">
     <meta name="theme-color" content="#ffffff">
 </head>
 <body>
