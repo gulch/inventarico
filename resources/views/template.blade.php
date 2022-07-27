da5d750422c83170b29c1fb8df72df8de75539ea
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2022-07-27 20:51:22 +0300

    minify Semantic css file, combine fonts and app css

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" type="text/css" href="/assets/vendor/semantic/2.4.2/semantic.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/fonts.css?v={{ config('app.version') }}">
+    <link rel="stylesheet" type="text/css" href="/assets/vendor/semantic/2.4.2/semantic.min.css">
 
     @if(isset($styles))
         @foreach($styles as $style)
