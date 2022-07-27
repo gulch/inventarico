a6ab0cfa46db61413e6431e38e8897b54b943fa5
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2022-07-27 15:26:40 +0300

    upgraded Flatpickr to v4.6.13

--- resources/views/partials/flatpickr.blade.php
+++ resources/views/partials/flatpickr.blade.php
@@ -1,15 +1,16 @@
 {{-- Flatpickr --}}
-<link rel="stylesheet" type="text/css" href="/assets/vendor/flatpickr/2.0.0/flatpickr.min.css">
+<link rel="stylesheet" type="text/css" href="/assets/vendor/flatpickr/4.6.13/flatpickr.min.css">
 
-<script defer src="/assets/vendor/flatpickr/2.0.0/flatpickr.min.js"></script>
-<script defer src="/assets/vendor/flatpickr/2.0.0/flatpickr.l10n.{{ config('app.locale') }}.js"></script>
+<script defer src="/assets/vendor/flatpickr/4.6.13/flatpickr.min.js"></script>
+<script defer src="/assets/vendor/flatpickr/4.6.13/{{ config('app.locale') }}.min.js"></script>
 
 <script>
     $(document).ready(function () {
         $('.datetimepicker').flatpickr({
             dateFormat: 'd.m.Y H:i',
             enableTime: true,
-            time_24hr: true
+            time_24hr: true,
+            locale: '{{ config('app.locale') }}',
         });
     });
 </script>
